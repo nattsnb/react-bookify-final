bc1630d6faa7893e75e78869b6f45455937c1723
Natasza ASzatan <natiye.nb@gmai.com>
2024-11-20 22:26:29 +0100

    added layout component and routes, copied font file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { ThemeProvider } from "@mui/material/styles";
-import {CssBaseline} from "@mui/material";
+import { CssBaseline } from "@mui/material";
 import theme from "./theme";
+import { Route, Routes } from "react-router-dom";
+import { Results } from "./pages/Results/index.jsx";
+import { Venue } from "./pages/Venue/index.jsx";
+import { Layout } from "./components/Layout/index.jsx";
 
 export function App() {
-
-
   return (
-      <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {/*<Layout>*/}
-          {/*    <Routes>*/}
-          {/*        <Route path="/results/" element={<Results />} exact />*/}
-          {/*        <Route path="/venue/" element={<Venue />} />*/}
-          {/*    </Routes>*/}
-          {/*</Layout>*/}
-      </ThemeProvider>
-  )
-
-        }
-
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Layout>
+        <Routes>
+          <Route path="/results/" element={<Results />} exact />
+          <Route path="/venue/" element={<Venue />} />
+        </Routes>
+      </Layout>
+    </ThemeProvider>
+  );
+}
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,9 @@
+export function Layout({ children }) {
+  return (
+    <>
+      <p>NAVBAR</p>
+      {children}
+      <p>FOOTER</p>
+    </>
+  );
+}
