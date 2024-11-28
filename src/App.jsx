51f2cb88242aa5abe866748ae7bab776a0fc3fbe
Natasza ASzatan <natiye.nb@gmai.com>
2024-11-29 00:28:23 +0100

    fixed import and path in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import theme from "./theme";
 import { Route, Routes } from "react-router-dom";
-import { Results } from "./pages/Results/index.jsx";
+import { Results } from "./pages/ResultsView/index.jsx";
 import { Venue } from "./pages/Venue/index.jsx";
 import { Layout } from "./components/Layout/index.jsx";
 import "./poppins.css";
@@ -13,7 +13,7 @@ export function App() {
       <CssBaseline />
       <Layout>
         <Routes>
-          <Route path="/results/" element={<Results />} exact />
+          <Route path="/" element={<Results />} exact />
           <Route path="/venue/:venueId" element={<Venue />} />
           <Route path="/aboutUs/" element={<p>about us</p>} />
           <Route path="/yourFavourites/" element={<p>your favourites</p>} />
