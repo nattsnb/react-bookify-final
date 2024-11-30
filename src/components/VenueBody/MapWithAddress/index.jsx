28d68fe1484775a0c3f85b46ce9a2e2620e7c56c
Natasza ASzatan <natiye.nb@gmai.com>
2024-11-30 14:21:03 +0100

    finished adding the map and styling buttons

--- src/components/VenueBody/MapWithAddress/index.jsx
+++ src/components/VenueBody/MapWithAddress/index.jsx
@@ -3,25 +3,37 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { StyledMapContainer } from "./MapWithAddress.styled.js";
 import { useMapWithAddress } from "./useMapWithAddress.js";
+import { VerticalContainer } from "../../../shared/styledComponents/verticalContainer.styled.js";
+import { CircularProgress } from "@mui/material";
+import "leaflet-fullscreen";
+import "leaflet-fullscreen/dist/leaflet.fullscreen.css";
 
 const MapWithAddress = ({ locationData }) => {
-  const { coordinatesData, isLoading, address } =
+  const { coordinatesData, isLoading, address, FullscreenControl } =
     useMapWithAddress(locationData);
-  console.log(coordinatesData);
-  console.log(isLoading);
-  console.log(address);
+  let lat = null;
+  let lng = null;
+
+  if (isLoading) {
+    return (
+      <VerticalContainer>
+        <CircularProgress />
+      </VerticalContainer>
+    );
+  } else {
+    lat = coordinatesData.results[0].geometry.lat;
+    lng = coordinatesData.results[0].geometry.lng;
+  }
+
   return (
     <StyledMapContainer>
-      ola
-      {/*<MapContainer center={[lat, lng]} zoom={13}>*/}
-      {/*  <TileLayer*/}
-      {/*      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"*/}
-      {/*      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'*/}
-      {/*  />*/}
-      {/*  <Marker position={[lat, lng]}>*/}
-      {/*    <Popup>{address}</Popup>*/}
-      {/*  </Marker>*/}
-      {/*</MapContainer>*/}
+      <MapContainer center={[lat, lng]} zoom={13}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[lat, lng]}>
+          <Popup>{address}</Popup>
+        </Marker>
+        <FullscreenControl />
+      </MapContainer>
     </StyledMapContainer>
   );
 };
--- src/components/VenueBody/MapWithAddress/useMapWithAddress.js
+++ src/components/VenueBody/MapWithAddress/useMapWithAddress.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { api } from "../../../shared/api.js";
+import { useMap } from "react-leaflet";
+import "leaflet-fullscreen";
+import "leaflet-fullscreen/dist/leaflet.fullscreen.css";
 
 export function useMapWithAddress(locationData) {
   const [coordinatesData, setCoordinatesData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-  const address = `${locationData.streetNumber} ${locationData.streetName}, ${locationData.postalCode}, ${locationData.city}`;
+  const address =
+    `${locationData.streetNumber} ${locationData.streetName}, ${locationData.postalCode}, ${locationData.city}`.toLowerCase();
 
   useEffect(() => {
     async function getCoordinatesData(address) {
@@ -24,5 +28,24 @@ export function useMapWithAddress(locationData) {
     coordinatesData,
     isLoading,
     address,
+    FullscreenControl,
   };
 }
+
+export const FullscreenControl = () => {
+  const map = useMap();
+
+  useEffect(() => {
+    const fullscreenControl = L.control.fullscreen({
+      position: "bottomright",
+    });
+
+    map.addControl(fullscreenControl);
+
+    return () => {
+      map.removeControl(fullscreenControl);
+    };
+  }, [map]);
+
+  return null;
+};
