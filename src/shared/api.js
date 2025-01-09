d6386d5aaad77d205ced99446e81029d149cfdb8
Natasza ASzatan <natiye.nb@gmai.com>
2025-01-09 19:46:51 +0100

    on the way to change data source to fake api on both ends

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,5 +1,5 @@
 const OPENCAGE_API_KEY = "938e7cda5d2c45f986a8c74c468eaaa7";
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = https://my-json-server.typicode.com/nattsnb/bookify-data-to-post/
 
 
 const getAllVenues = () => {
